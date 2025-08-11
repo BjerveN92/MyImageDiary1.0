85d58f7ab9f3ddf8d201128fcae2b96925371ca7
Martin Bjervig <martinbjervig@gmail.com>
2025-08-11 12:45:46 +0200

    implementet cloudinary configuration into the project

--- src/main/java/com/example/MyImageDiary10/config/CloudinaryConfig.java
+++ src/main/java/com/example/MyImageDiary10/config/CloudinaryConfig.java
@@ -1,5 +1,28 @@
 package com.example.MyImageDiary10.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+@Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+    @Value("${cloudinary.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", secretKey));
+
+    }
 }
