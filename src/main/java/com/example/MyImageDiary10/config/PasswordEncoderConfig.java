286d2cec51f2c55ce347682ee5a691bd123e80f1
Martin Bjervig <martinbjervig@gmail.com>
2025-08-11 15:31:21 +0200

    added the rest of the required config-classes

--- src/main/java/com/example/MyImageDiary10/config/PasswordEncoderConfig.java
+++ src/main/java/com/example/MyImageDiary10/config/PasswordEncoderConfig.java
@@ -1,5 +1,14 @@
 package com.example.MyImageDiary10.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoderConfig {
 
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/example/MyImageDiary10/config/WebConfig.java
+++ src/main/java/com/example/MyImageDiary10/config/WebConfig.java
@@ -1,5 +1,30 @@
 package com.example.MyImageDiary10.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// ===== IMPORTANT INFO ===== // 
+// This configuration will probably face some changes during the buildup for this projects //
+
+@Configuration
+public class WebConfig {
+    @Bean
+
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**") // allows all endpoints
+                        .allowedOrigins("http://localhost:3000") // localhost to Frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // behövs vid senare tillfälle om web tokens skall användas
+            }
+        };
+    }
 }
