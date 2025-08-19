43a2b3d985d468a65fc547ff3e3ea63f6ad00451
Martin Bjervig <martinbjervig@gmail.com>
2025-08-19 12:07:41 +0200

    user enitity implemented

--- src/main/java/com/example/MyImageDiary10/models/User.java
+++ src/main/java/com/example/MyImageDiary10/models/User.java
@@ -1,5 +1,27 @@
 package com.example.MyImageDiary10.models;
 
-public class User {
+import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+
+    private String username;
+    private String Email;
+    private String password;
+
+    private List<Entry> entries;
 }
