77bca3dc781bee81cab288c98c92ca21db82ad64
Martin Bjervig <martinbjervig@gmail.com>
2025-08-19 12:47:13 +0200

    Entry implemented + modifying small changes in user

--- src/main/java/com/example/MyImageDiary10/models/Entry.java
+++ src/main/java/com/example/MyImageDiary10/models/Entry.java
@@ -1,5 +1,29 @@
 package com.example.MyImageDiary10.models;
 
-public class Entry {
+import java.time.LocalDate;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Document(collection = "entries")
+public class Entry {
+    @Id
+    private String id;
+
+    private String title;
+    private String content;
+    private String entryImageURL;
+    private LocalDate createdAt; // This will be handeled by Json formatter later on when decided what format i
+                                 // will use
+
+    private String userId; // refering to the user-model
 }
