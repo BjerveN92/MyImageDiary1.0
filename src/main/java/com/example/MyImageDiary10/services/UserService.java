002750dffd7dc03062ddfa43291007dc8f41b993
Martin Bjervig <martinbjervig@gmail.com>
2025-08-20 22:00:52 +0200

    implemented update-method in UserService

--- src/main/java/com/example/MyImageDiary10/services/UserService.java
+++ src/main/java/com/example/MyImageDiary10/services/UserService.java
@@ -33,7 +33,22 @@ public class UserService extends ObjectService<UserResponse> {
     // update the user by Id
     @Override
     public UserResponse update(String id, UserResponse existingUser) {
-        return null;
+        return userRepository.findById(id)
+                .map(user -> {
+                    // Update userfileds
+                    user.setUsername(existingUser.getUsername());
+                    user.setProfileImageURL(existingUser.getProfileImageURL());
+                    // Save the updated user
+                    userRepository.save(user);
+
+                    // Return the updated user
+                    return UserResponse.builder()
+                            .id(user.getId())
+                            .username(user.getUsername())
+                            .profileImageURL(user.getProfileImageURL())
+                            .build();
+                })
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
     // delete one user
